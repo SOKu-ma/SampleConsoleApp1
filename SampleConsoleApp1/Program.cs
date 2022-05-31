0c1a522d18894e306672cb4d8a1668e17b352409
otashuuhei <uverrevu812@gmail.com>
2022-06-01 00:19:30 +0900

    FizzBuzz問題の実装

--- SampleConsoleApp1/Program.cs
+++ SampleConsoleApp1/Program.cs
@@ -2,12 +2,45 @@
 
 namespace SampleConsoleApp1
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        /// 定数
+        const string conFizz = "Fizz";
+        const string conBuzz = "Buzz";
+        const string conFizzBuzz = "FizzBuzz";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// FizzBuzz問題
+        /// </summary>
+        /// <remarks>
+        /// 3で割り切れる：Fizz
+        /// 5で割り切れる：Buzz
+        /// 15で割り切れる：FizzBuzz
+        /// </remarks>
+        /// <param name="num">整数値</param>
+        public static String FizzBuzz(int num)
+        {
+            if (num % 15 == 0)
+            {
+                return conFizzBuzz;
+            }
+            else if (num % 5 == 0)
+            {
+                return conBuzz;
+            }
+            else
+            {
+                return conFizz;
+            }
+        }
     }
 }
-
--- SampleTestConsoleApp1/UnitTest1.cs
+++ SampleTestConsoleApp1/UnitTest1.cs
@@ -1,18 +1,80 @@
 ﻿using NUnit.Framework;
+using SampleConsoleApp1;
 
 namespace SampleTestConsoleApp1
 {
+
+    /// <summary>
+    /// テストクラス
+    /// </summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        /// 定数
+        const string Fizz = "Fizz";
+        const string Buzz = "Buzz";
+        const string FizzBuzz = "FizzBuzz";
+
+        ///// <summary>
+        ///// 初期処理
+        ///// </summary>
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        ///// <summary>
+        ///// 終了処理
+        ///// </summary>
+        //[TearDown]
+        //public void Final()
+        //{
+        //}
+
+        /// <summary>
+        /// 3で割り切れる数字はFizz
+        /// </summary>
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(12)]
+        public void Num3_Fizz(int num)
         {
+            string ret = Program.FizzBuzz(num);
+            Assert.AreEqual(Fizz, ret);
         }
 
-        [Test]
-        public void Test1()
+        /// <summary>
+        /// 5で割り切れる数字はBuzz
+        /// </summary>
+        [TestCase(5)]
+        [TestCase(10)]
+        public void Num5_Buzz(int num)
         {
-            Assert.Pass();
+            string ret = Program.FizzBuzz(num);
+            Assert.AreEqual(Buzz, ret);
         }
+
+        /// <summary>
+        /// 15で割り切れる数字はFizzBuzz
+        /// </summary>
+        [TestCase(15)]
+        public void Num15_FizzBuzz(int num)
+        {
+            string ret = Program.FizzBuzz(num);
+            Assert.AreEqual(FizzBuzz, ret);
+        }
+
+        ///// <summary>
+        ///// 計算テスト
+        ///// </summary>
+        ///// <param name="a"></param>
+        ///// <param name="b"></param>
+        ///// <param name="c"></param>
+        //[TestCase(1, 2, 3)]
+        //[TestCase(4, 5, 9)]
+        //public void Calc_Test(int a, int b, int c)
+        //{
+        //    Assert.AreEqual(c, a + b);
+        //}
     }
 }
