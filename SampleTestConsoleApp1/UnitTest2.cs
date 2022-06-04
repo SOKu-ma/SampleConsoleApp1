4f4103658f929be9229fa86002dd97c670f6331f
otashuuhei <uverrevu812@gmail.com>
2022-06-04 22:14:55 +0900

    2倍チェック問題を追加

--- SampleTestConsoleApp1/UnitTest2.cs
+++ SampleTestConsoleApp1/UnitTest2.cs
@@ -1,9 +1,10 @@
 ﻿using NUnit.Framework;
+using SampleConsoleApp1;
 
 namespace SampleTestConsoleApp1
 {
 	/// <summary>
-    /// 
+    /// 2倍チェック問題
     /// </summary>
 	public class Tests2
 	{
@@ -13,7 +14,6 @@ namespace SampleTestConsoleApp1
         [SetUp]
 		public void setUp()
 		{
-
 		}
 
 		/// <summary>
@@ -22,8 +22,58 @@ namespace SampleTestConsoleApp1
 		[TearDown]
 		public void Final()
         {
-
         }
-	}
-}
 
+        /// <summary>
+        /// 正常系
+        /// </summary>
+        /// <remarks>
+        /// 数字3桁：入力値の2倍の値を出力
+        /// </remarks>
+        [TestCase("678")]
+        public void inputNum(string strNum)
+        {
+            // 入力値の2倍と2倍チェックメソッドの返り値を比較
+            Assert.AreEqual((int.Parse(strNum) * 2).ToString(), Program2.chekcNum(strNum));
+        }
+
+        /// <summary>
+        /// 異常系
+        /// </summary>
+        /// <remarks>
+        /// アルファベット3文字："error"を出力
+        /// </remarks>
+        [TestCase("abc")]
+        public void inputAlphabet(string str)
+        {
+            // 2倍チェックメソッドの返り値が"error"であることを確認
+            Assert.AreEqual("error", Program2.chekcNum(str));
+        }
+
+        /// <summary>
+        /// 異常系
+        /// </summary>
+        /// <remarks>
+        /// 数字と英小文字が混ざった値:"error"を出力
+        /// </remarks>
+        [TestCase("0x8")]
+        public void inputNumAndStr(string str)
+        {
+            // 2倍チェックメソッドの返り値が"error"であることを確認
+            Assert.AreEqual("error", Program2.chekcNum(str));
+        }
+
+        /// <summary>
+        /// 正常系
+        /// </summary>
+        /// <remarks>
+        /// 数字3桁（0始まり）：入力値の2倍の値を出力
+        /// </remarks>
+        [TestCase("024")]
+        public void inputZeroNum(string strNum)
+        {
+            // 入力値の2倍と2倍チェックメソッドの返り値を比較
+            Assert.AreEqual((int.Parse(strNum) * 2).ToString(), Program2.chekcNum(strNum));
+        }
+    }
+}
