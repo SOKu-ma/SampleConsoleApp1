3abf829fee8a1937484d3860449f0cf58d6b99c3
otashuuhei <uverrevu812@gmail.com>
2022-06-19 15:17:30 +0900

    2倍チェック問題の修正、増減管理問題の追加

--- SampleConsoleApp1/Program2.cs
+++ SampleConsoleApp1/Program2.cs
@@ -1,41 +1,68 @@
 ﻿using System;
 namespace SampleConsoleApp1
 {
-	/// <summary>
-    /// 
+    /// <summary>
+    /// 2倍チェック問題
     /// </summary>
-	public static class Program2
+    public static class Program2
 	{
 
-        ///// <summary>
-        ///// 提出版
-        ///// </summary>
-        //public static void TestAtCoder()
-        //{
-        //    // インプット値を取得
-        //    String str = Console.ReadLine().ToString();
+        /// <summary>
+        /// 提出版
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        public static void TestAtCoder()
+        {
 
-        //    int i = 0;
-        //    bool checkInt = int.TryParse(str, out i);
+            // インプット値を取得
+            String str = Console.ReadLine().ToString();
+            int i = 0;
+            bool checkInt = int.TryParse(str, out i);
 
-        //    // 整数値の場合
-        //    if (i != 0)
-        //    {
-        //        // インプット値の2倍の数値を出力
-        //        Console.WriteLine((int.Parse(str) * 2).ToString());
-        //    }
-        //    // 数値以外（数字含む文字列も含む）の場合
-        //    else
-        //    {
-        //        Console.WriteLine("error");
-        //    }
-        //}
+            // 整数値の場合
+            if (checkInt)
+            {
+                // 0の場合は0を返す
+                if (int.Parse(str) == 0)
+                {
+                    Console.WriteLine(0.ToString());
+                }
+                else
+                {
+                    Console.WriteLine((int.Parse(str) * 2).ToString());
+                }
+            }
+            // 数値以外（文字列含む）
+            else
+            {
+                Console.WriteLine("error");
+            }
 
-		/// <summary>
+            //// インプット値を取得
+            //String str = Console.ReadLine().ToString();
+
+            //int i = 0;
+            //bool checkInt = int.TryParse(str, out i);
+
+            //// 整数値の場合
+            //if (i != 0)
+            //{
+            //    // インプット値の2倍の数値を出力
+            //    Console.WriteLine((int.Parse(str) * 2).ToString());
+            //}
+            //// 数値以外（数字含む文字列も含む）の場合
+            //else
+            //{
+            //    Console.WriteLine("error");
+            //}
+        }
+
+        /// <summary>
         /// Mainメソッド
         /// </summary>
         /// <param name="args"></param>
-		public static void Main2(string[] args)
+        public static void Main2(string[] args)
 		{
 			Console.WriteLine("2倍チェック問題");
 		}
---
+++ SampleTestConsoleApp1/UnitTest3.cs
@@ -0,0 +1,37 @@
+﻿using SampleConsoleApp1;
+using NUnit.Framework;
+
+namespace SampleTestConsoleApp3
+{
+	/// <summary>
+    /// 増減管理
+    /// </summary>
+	public class IncreaseDecreaseManagementTest
+	{
+
+		/// <summary>
+        /// 初期処理
+        /// </summary>
+        [SetUp]
+		public void setUp()
+		{
+		}
+
+		/// <summary>
+        /// 終了処理
+        /// </summary>
+        [TearDown]
+		public void tearDown()
+        {
+        }
+
+        /// <summary>
+        /// 増減管理
+        /// </summary>
+        [TestCase()]
+        public void increDecreMng()
+        {
+        }
+	}
+}
+
