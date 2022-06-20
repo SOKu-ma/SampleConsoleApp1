5dbdf1382466d6c2983714f359c81955b5fc8723
otashuuhei <uverrevu812@gmail.com>
2022-06-20 23:47:53 +0900

    3番目検索問題の実装中

--- SampleConsoleApp1/Program3.cs
+++ SampleConsoleApp1/Program3.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 namespace SampleConsoleApp1
 {
-	public class IncreaseDecreaseManagement
+	public class Program3
 	{
 		/// <summary>
-        /// 
+        /// 3番目検索問題
         /// </summary>
-		public void Main()
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        /// <param name="f"></param>
+        /// <remarks></remarks>
+		public static int Main(int a, int b, int c, int d, int e, int f)
         {
+            return 0;
         }
 	}
 }
-
